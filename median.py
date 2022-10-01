ea9ce70f5e40302ba7fe409c08107ea95c934e71
Tom Hurford <hurford.t0@gmail.com>
2022-10-01 14:19:20 +0100

    011022 - TGH - Task Complete

--- median.py
+++ median.py
@@ -1,12 +1,25 @@
-"""Median calculator."""
-"""ENTER YOUR SOLUTION HERE!"""
+""" Request a list of comma separated numbers from the user and return the median. """
 
-while True:
-    try:
-        print("Enter a list of numbers separated by commas: ")
-        numbers = [float(value) for value in input().split(",")]
-    except ValueError:
-        print("Some input could not be converted to a number!")
+# Import modules
+import sys
+
+# Define functions
+def median(numbers):
+    """Return the median of a list of numbers."""
+    numbers.sort()
+    if len(numbers) % 2 == 0:
+        median = (numbers[int(len(numbers)/2)] + numbers[int(len(numbers)/2)-1]) / 2
     else:
-        break
-print(numbers)
+        median = numbers[int(len(numbers)/2)]
+    return median
+
+# Main
+if __name__ == "__main__":
+    # Get user input
+    numbers = input("Enter a comma separated list of numbers: ")
+    # Convert user input to list of integers
+    numbers = [int(n) for n in numbers.split(",")]
+    # Print median
+    print("The median is: {}".format(median(numbers)))
+
+# End of script
